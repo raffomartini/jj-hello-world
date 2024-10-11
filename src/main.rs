7135847b78d488364b4bf966ac74572c6830fd12
Raffo Martini <raffaello.martini@gmail.com>
2024-10-11 13:55:48 +0100

    create hello and goodbye functions.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 /// A "Hello, world!" program.
+/// 
+/// New comments here.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
     println!("Goodbye, world.");
-    // Random comment just because.
 }
